ae8ba8dd1bd817b86724e3252bec6db631da3d4d
Glouby <hoaraueloan0@gmail.com>
2023-10-31 15:59:53 +0100

    maintenant

--- partials/head.php
+++ partials/head.php
@@ -2,7 +2,7 @@
 <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title><?php $titre ?></title>
+        <title> Acceuil </title>
         <link rel="stylesheet" href="<?= PROJECT_FOLDER ?>assets/css/main.css">
         <link rel="stylesheet" href="<?= PROJECT_FOLDER ?>assets/css/header.css">
         <link rel="stylesheet" href="<?= PROJECT_FOLDER ?>assets/css/footer.css">
