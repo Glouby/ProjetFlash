e74d1d37ea5fed36740653f055e85ab75f1c409b
Glouby <hoaraueloan0@gmail.com>
2023-11-02 13:31:47 +0100

    story_7

--- login.php
+++ login.php
@@ -1,7 +1,19 @@
 <?php
 require 'utils/common.php';
+require 'utils/database.php';
+
 $NamePage = 'login';
 
+if (!empty($_GET["email"])){
+    $email = $_GET["email"];
+}else{
+    $email = null;
+}
+if (!empty($_GET["password"])){
+    $password = $_GET["password"];
+}else{
+    $password = null;
+}
 ?>
 
 
@@ -22,16 +34,16 @@ $NamePage = 'login';
     </div>   
 
   <!-- les informations pour la connexion-->
-    <form action="info"> 
+    <form method="get"> 
         <div class="Space">
             <div class="input">
-                <input class="id-connexion" type="text" name="Email" id="Email" placeholder="Email">
+                <input class="id-connexion" type="text" name="email" id="Email" placeholder="Email">
             </div>
             
             <!-- mdp -->
 
             <div class="input">
-                <input class="id-connexion" type="password" name="Mot de passe" id="mdp" placeholder="Mot de passe">
+                <input class="id-connexion" type="password" name="password" id="mdp" placeholder="Mot de passe">
             </div>
 
             <div class="input-bouton">
@@ -40,10 +52,11 @@ $NamePage = 'login';
             </div>  
 
         </div>             
-        <?php
+    </form>
+
+    <?php
         require 'partials/footer.php';
     ?>
-    </form>
     <a href="#" class="le_btn">^</a>
     
 </body>
