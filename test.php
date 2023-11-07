84101cfeab792eafba4a010d7902dbb14d452308
Glouby <hoaraueloan0@gmail.com>
2023-11-07 10:56:10 +0100

    kuhoiunj

--- test.php
+++ test.php
@@ -1,18 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
-    <title>Rotation d'image avec affichage d'une autre image</title>
-</head>
-<body>
-    <div class="image-container">
-        <div class="image-wrapper">
-            <img src="assets/image/carte.png" class="image-1" id="image1">
-            <img src="assets/image/Arnold.jpeg" class="image-2" id="image2">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Memory</title>
+
+    <link
+      rel="stylesheet"
+      href="styles.css" />
+    <script
+      src="script.js"
+      defer></script>
+  </head>
+
+  <body>
+    <div class="game">
+      <div class="controls">
+        <button>Début</button>
+        <div class="stats">
+          <div class="moves">0 mouvement</div>
+          <div class="timer">time: 0 sec</div>
         </div>
+      </div>
+      <div class="board-container">
+        <div class="board" data-dimension="4"></div>
+        <div class="win">Tu as gagné!</div>
+      </div>
     </div>
-    <script src="script.js"></script>
-</body>
+  </body>
 </html>
